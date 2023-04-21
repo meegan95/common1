27ce47da22b2755230313bfca823825cb3da3efa
meegan95 <122483262+meegan95@users.noreply.github.com>
2023-04-21 17:46:12 +0300

    add mapping

--- eureka-client/src/main/java/com/example/eurekaclient/controllers/RestController.java
+++ eureka-client/src/main/java/com/example/eurekaclient/controllers/RestController.java
@@ -1,8 +1,10 @@
 package com.example.eurekaclient.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @org.springframework.web.bind.annotation.RestController
+@RequestMapping("/main")
 public class RestController {
     @GetMapping("/test")
     public String test(){
