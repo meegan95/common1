165c05335e35fcaf5d1f829484cfb813aa16e7a3
meegan95 <122483262+meegan95@users.noreply.github.com>
2023-04-23 12:50:56 +0300

    add config-server

---
+++ aa-config-client/src/main/java/com/example/aaconfigclient/AaConfigClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.aaconfigclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AaConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AaConfigClientApplication.class, args);
+    }
+
+}
---
+++ aa-config-client/src/test/java/com/example/aaconfigclient/AaConfigClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aaconfigclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AaConfigClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
