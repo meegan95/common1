8de62b3fd7a1d7191876f1268e7f0093b3204453
meegan95 <122483262+meegan95@users.noreply.github.com>
2023-04-28 13:47:08 +0300

    add tracing to client-service

--- client-service/src/main/java/com/example/clientservice/connector/BookServiceConnector.java
+++ client-service/src/main/java/com/example/clientservice/connector/BookServiceConnector.java
@@ -3,9 +3,14 @@ package com.example.clientservice.connector;
 
 import com.example.clientservice.config.FeignConfig;
 import com.example.clientservice.model.Book;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.openfeign.FallbackFactory;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Collections;
 import java.util.List;
 
 @FeignClient(
@@ -18,3 +23,15 @@ public interface BookServiceConnector {
     @GetMapping("/api/books")
     List<Book> getAllBooks();
 }
+
+
+@Component
+class Fallback implements BookServiceConnector {
+    @Override
+    public List<Book> getAllBooks() {
+        System.out.println("Fallback works");
+        return Collections.emptyList();
+    }
+}
+
+
